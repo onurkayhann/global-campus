e402dbc782a3d794c9d8ea8310a1602c00db19ea
Onur <onur.kayhan90@gmail.com>
2025-01-29 12:35:39 +0100

    Initial Commit

---
+++ GlobalCampus/GlobalCampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalCampusApp.swift
+//  GlobalCampus
+//
+//  Created by Onur Kayhan on 2025-01-29.
+//
+
+import SwiftUI
+
+@main
+struct GlobalCampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
